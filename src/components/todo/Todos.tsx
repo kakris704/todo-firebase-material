75f83584395ed0e6b75852379002baeee65e0ff3
kakris704 <kakris704@gmail.com>
2024-05-26 00:14:29 +0900

    テキストフィールドの追加・見た目の改善

--- src/components/todo/Todos.tsx
+++ src/components/todo/Todos.tsx
@@ -1,18 +1,41 @@
-import { ModeEdit } from '@mui/icons-material'
-import { AppBar, IconButton, Stack, Typography } from '@mui/material'
+import { ModeEdit, Add, TaskAlt } from '@mui/icons-material'
+import { AppBar, IconButton, InputBase, Paper, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const Todos = () => {
   return (
-    <div className='todos grid-item'>
+    <div className='todos grid-item' style={{position:'relative'}}>
         <AppBar position="static" sx={{padding: '7px'}} color='primary'>
           <Stack direction='row' sx={{justifyContent:'space-between'}}>
-            <Typography variant='h6' sx={{alignItems:'center', display: 'flex'}}>やることリスト</Typography>
+            <div className="title-wrapper">
+              <TaskAlt />
+              <Typography variant='h6' sx={{alignItems:'center', display: 'flex'}}>やることリスト</Typography>
+            </div>
             <IconButton>
               <ModeEdit sx={{color:'white'}}/>
             </IconButton>
           </Stack>
         </AppBar>
+        <div className="text-field-wrapper">
+          <Paper sx={{width: '95%', height:'50px', position: 'absolute', bottom:'30px'}} elevation={3}>
+            <Stack direction='row' sx={{display:'flex', alignItems:'center', justifyContent:'space-between'}}>
+              <InputBase
+                placeholder="Todoを追加"
+                inputProps={{ 'aria-label': 'Todoを追加する' }}
+                sx={{height: '100%', width: '100%', '.MuiInputBase-input': {
+                  height: '45px',
+                  fontSize: '1.3rem',
+                  pl: '10px'
+                }}}
+              />
+              <IconButton sx={{width:'40px', height:'40px', mr:'5px'}}>
+                <Add fontSize='medium'/>
+              </IconButton>
+            </Stack>
+          </Paper>
+          
+        </div>
+        
     </div>
   )
 }
